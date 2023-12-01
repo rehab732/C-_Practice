fb4efd67f28c05a3ddf2f0196a722f40fd07c233
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-12-01 23:52:12 +0200

    add region

--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -4,13 +4,14 @@
 	{
 		static void Main(string[] args)
 		{
-			var ip = new Indexer();
-			ip[0] = 122;
-			ip[1] = 133;
-			ip[2] = 144;
-			ip[3] = 155;
-
-			Console.WriteLine(ip.GetIP);
+			#region Indexer
+			//var ip = new Indexer();
+			//ip[0] = 122;
+			//ip[1] = 133;
+			//ip[2] = 144;
+			//ip[3] = 155;
+			//Console.WriteLine(ip.GetIP);
+			#endregion
 		}
 	}
 }
