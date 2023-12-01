ce8a103ef3cb939a2426b6e3af7d89d3fb8abb77
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-12-01 23:50:14 +0200

    indexer

---
+++ Practice/Practice/Indexer.cs
@@ -0,0 +1,27 @@
+﻿namespace Practice
+{
+	public class Indexer
+	{
+		private int[] ip=new int[4];
+
+        public int this[int index]
+        {
+            get
+            {
+                return ip[index];
+            }
+            set
+            {
+                ip[index] = value;
+            }
+        }
+        //public Indexer(int seg1,int seg2,int seg3, int seg4)
+        //{
+        //    ip[0] = seg1;
+        //    ip[1] = seg2;
+        //    ip[2] = seg3;
+        //    ip[3] = seg4;
+        //}
+        public string GetIP=>string.Join(".", ip);
+    }
+}
---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Practice
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			var ip = new Indexer();
+			ip[0] = 122;
+			ip[1] = 133;
+			ip[2] = 144;
+			ip[3] = 155;
+
+			Console.WriteLine(ip.GetIP);
+		}
+	}
+}
